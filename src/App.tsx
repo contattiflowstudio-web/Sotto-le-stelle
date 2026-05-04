bc06084200d5ac37b5dd6894dd1be997c8750512
contattiflowstudio-web <contattiflowstudio@gmail.com>
2026-05-04 19:28:28 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter BASENAME = “/Sotto-le-stelle”> 
+      <BrowserRouter basename = "/Sotto-le-stelle">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
